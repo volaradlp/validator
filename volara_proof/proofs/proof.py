d48b06e1df894a7797c7aa3fba17c023f4bcff0a
Volara <set>
2024-12-11 15:18:03 -0500

    Add flow for user validation

--- volara_proof/proofs/proof.py
+++ volara_proof/proofs/proof.py
@@ -1,19 +1,26 @@
 import copy
 
-from volara_proof.extract import extract_data
+from volara_proof.extract import extract_data, extract_user_data
 from volara_proof.proofs.proof_of_quality import proof_of_quality
 from volara_proof.models.proof_response import ProofResponse
 from volara_proof.models.proof_config import ProofConfig
 
 from volara_proof.storage.rewards import RewardsStorage
+from volara_proof.storage.user_info import UserInfoStorage
 
 rewards_storage = RewardsStorage()
+user_info_storage = UserInfoStorage()
 
 
 def proof(
     input_file: str, proof_response: ProofResponse, config: ProofConfig
 ) -> ProofResponse:
     proof_response = copy.deepcopy(proof_response)
+    user_data = extract_user_data(input_file)
+    if user_data is not None:
+        proof_response.valid = True
+        proof_response.score = 0
+        return proof_response
     tweets_data = extract_data(input_file)
     is_valid, file_score, tweet_info, unique_tweets, total_tweets = (
         proof_of_quality(tweets_data, config.file_id, config)
---
+++ volara_proof/storage/user_info.py
@@ -0,0 +1,26 @@
+import logging
+import os
+import requests
+
+from volara_proof.models.user_data import UserData
+from volara_proof.constants import VOLARA_API_URL
+
+
+class UserInfoStorage:
+    def verify_user(self, user_info: UserData) -> bool:
+        request_body = {
+            "handle": user_info.handle,
+            "walletAddress": user_info.wallet_address,
+        }
+        try:
+            resp = requests.post(
+                url=f"{VOLARA_API_URL}/v1/validator/validate-user",
+                json=request_body,
+                headers={"Authorization": f"Bearer {os.environ['VOLARA_API_KEY']}"},
+            )
+            resp.raise_for_status()
+            logging.info("Succesfully determined if user exists.")
+            return resp.json()["userValidated"]
+        except Exception:
+            logging.exception("[CRITICAL FAILURE] Failed to determine if user exists.")
+            raise
